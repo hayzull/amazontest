764f4cde963ce5b2969b6f65cd347903ebb14e9b
Hazel Esmilla <hesmilla8201@gmail.com>
2016-01-19 23:06:52 -0800

    python file moved into folder

---
+++ amazon.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+print('Input your Amazon email address')
+userEmail = input()
+print('Input your Amazon password')
+userPassword = input()
+
+browser = webdriver.Firefox()
+browser.get('www.amazon.com/sign-in')
+
+loginElem = browser.find_element_by_id('nav-link-yourAccount')
+loginElem.click()
+
+emailElem = browser.find_element_by_id('ap_email')
+emailElem.send_keys (userEmail)
+
+passwordElem = browser.find_element_by_id('ap_password')
+passwordElem.send_keys (userPassword)
+passwordElem.submit()
